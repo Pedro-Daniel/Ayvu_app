a52ec45acabf9ea89264853edf7223812c99bdf2
Pedro-Daniel <pedrobomtempo2001@gmail.com>
2025-01-21 12:01:43 -0300

    Coloquei flutterviz pages no padrão

--- tests/flutterviz/side_panel.dart
+++ tests/flutterviz/side_panel.dart
@@ -1,137 +1,85 @@
-///File download from FlutterViz- Drag and drop a tools. For more details visit https://flutterviz.io/
-
-///File download from FlutterViz- Drag and drop a tools. For more details visit https://flutterviz.io/
-
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SidePanel(),
+    );
+  }
+}
 
 class SidePanel extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            _buildMenuItem(
+              context,
+              icon: Icons.account_circle,
+              label: "My profile",
+              onTap: () {},
+            ),
+            _buildMenuItem(
+              context,
+              icon: Icons.keyboard_voice,
+              label: "My recordings",
+              onTap: () {},
+            ),
+            _buildMenuItem(
+              context,
+              icon: Icons.settings,
+              label: "Settings",
+              onTap: () {},
+            ),
+            _buildMenuItem(
+              context,
+              icon: Icons.code,
+              label: "About devs",
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
-@override
-Widget build(BuildContext context) {
-return Scaffold(
-backgroundColor: Color(0xffffffff),
-body:Align(
-alignment:Alignment.center,
-child:
-Column(
-mainAxisAlignment:MainAxisAlignment.center,
-crossAxisAlignment:CrossAxisAlignment.center,
-mainAxisSize:MainAxisSize.max,
-children: [
-Padding(
-padding:EdgeInsets.symmetric(vertical: 10,horizontal:0),
-child:
-Column(
-mainAxisAlignment:MainAxisAlignment.start,
-crossAxisAlignment:CrossAxisAlignment.center,
-mainAxisSize:MainAxisSize.max,
-children: [
-IconButton(
-icon:Icon(
-Icons.account_circle
-),
-onPressed:(){},
-color:Color(0xff212435),
-iconSize:50,
-),
-Text(
-"My profile",
-textAlign: TextAlign.start,
-overflow:TextOverflow.clip,
-style:TextStyle(
-fontWeight:FontWeight.w400,
-fontStyle:FontStyle.normal,
-fontSize:14,
-color:Color(0xff000000),
-),
-),
-],),),
-Padding(
-padding:EdgeInsets.symmetric(vertical: 10,horizontal:0),
-child:
-Column(
-mainAxisAlignment:MainAxisAlignment.start,
-crossAxisAlignment:CrossAxisAlignment.center,
-mainAxisSize:MainAxisSize.max,
-children: [
-IconButton(
-icon:Icon(
-Icons.keyboard_voice
-),
-onPressed:(){},
-color:Color(0xff212435),
-iconSize:50,
-),
-Text(
-"My recordings",
-textAlign: TextAlign.start,
-overflow:TextOverflow.clip,
-style:TextStyle(
-fontWeight:FontWeight.w400,
-fontStyle:FontStyle.normal,
-fontSize:14,
-color:Color(0xff000000),
-),
-),
-],),),
-Padding(
-padding:EdgeInsets.symmetric(vertical: 10,horizontal:0),
-child:
-Column(
-mainAxisAlignment:MainAxisAlignment.start,
-crossAxisAlignment:CrossAxisAlignment.center,
-mainAxisSize:MainAxisSize.max,
-children: [
-IconButton(
-icon:Icon(
-Icons.settings
-),
-onPressed:(){},
-color:Color(0xff212435),
-iconSize:50,
-),
-Text(
-"Settings",
-textAlign: TextAlign.start,
-overflow:TextOverflow.clip,
-style:TextStyle(
-fontWeight:FontWeight.w400,
-fontStyle:FontStyle.normal,
-fontSize:14,
-color:Color(0xff000000),
-),
-),
-],),),
-Padding(
-padding:EdgeInsets.symmetric(vertical: 10,horizontal:0),
-child:
-Column(
-mainAxisAlignment:MainAxisAlignment.start,
-crossAxisAlignment:CrossAxisAlignment.center,
-mainAxisSize:MainAxisSize.max,
-children: [
-IconButton(
-icon:Icon(
-Icons.code
-),
-onPressed:(){},
-color:Color(0xff212435),
-iconSize:50,
-),
-Text(
-"About devs",
-textAlign: TextAlign.start,
-overflow:TextOverflow.clip,
-style:TextStyle(
-fontWeight:FontWeight.w400,
-fontStyle:FontStyle.normal,
-fontSize:14,
-color:Color(0xff000000),
-),
-),
-],),),
-],),),
-)
-;}
+  Widget _buildMenuItem(BuildContext context,
+      {required IconData icon, required String label, required VoidCallback onTap}) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          IconButton(
+            icon: Icon(icon),
+            onPressed: onTap,
+            color: Theme.of(context).colorScheme.primary,
+            iconSize: 50,
+          ),
+          Text(
+            label,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.w400,
+              fontSize: 14,
+              color: Theme.of(context).colorScheme.onBackground,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
